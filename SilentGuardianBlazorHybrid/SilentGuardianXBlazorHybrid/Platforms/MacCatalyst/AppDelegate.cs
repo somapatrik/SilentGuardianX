f121ab5f60871ab5d6e203335e1ee99a261ac659
Patrik Šoma <soma.patrik@gmail.com>
2025-12-22 14:03:15 +0100

    Blazor Hybrit project added

---
+++ SilentGuardianBlazorHybrid/SilentGuardianXBlazorHybrid/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SilentGuardianXBlazorHybrid
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
